371c3ee64c9d62c0e549f1a7128d88dea667fd06
dasartini <adriansartinid@gmail.com>
2024-08-07 09:45:02 +0100

    learned extentions

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,9 +6,14 @@ addProduct({
 id: '1',
 title: "Vinto can",
 createdAt: new Date(),
+updatedAt: new Date(),
 stock: 90,
-category : {
-    id: '2',
-    name : 'Drinks'
+category: {
+    id: '20',
+  name: "Drinks",
+  createdAt: new Date(),
+  updatedAt: new Date(),
 }
+
+
 })
--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,12 +1,11 @@
 import { Category } from "../categories/categories.model";
+import { BaseModel } from "../users/base.model";
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 
 
-export interface Product {
-    id: string | number,
-    title: string ,
-    createdAt: Date ,
+export interface Product extends BaseModel {
     stock: number,
+    title: string ,
     size?: Sizes,
     category : Category
 }
